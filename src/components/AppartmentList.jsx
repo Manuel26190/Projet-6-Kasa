e891fe92fd883dbaa4b7c2c2599cf0277bf87550
Manuel Faure <manuelfaure26190@gmail.com>
2023-06-27 22:46:45 +0200

    save

--- src/components/AppartmentList.jsx
+++ src/components/AppartmentList.jsx
@@ -2,28 +2,21 @@ import Cart from "./Cart";
 import data from '../datas/data.js';
 import '../styles/Appartment.css';
 
-console.log(data)
+//console.log(data)
 
-function AppartmentList ({title, cover, id}) {
-    /*const title = data.map((appartment) => {
-        return appartment.title
-    });
-    const cover = data.map((appartments) => {
-        return appartments.cover
-    });
-*/
+function AppartmentList () {
+    
     return (
-        <ul className="lmj-plant-list">
+        <ul className="ul-cart" >
             {data.map(({title, cover, id}) => 
-                <Cart
-                title = {title}
-                cover = {cover}
+                <div className="div-cart" key={id}>
+                    <Cart
+                    title = {title}
+                    cover = {cover}
             />
-            )}
-            
+                </div>                
+            )}            
         </ul>
-            
-        
     )
 }
 
