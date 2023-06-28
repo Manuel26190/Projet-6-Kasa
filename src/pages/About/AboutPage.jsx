d87ceed5dda678af04defd2cb60c0c1251f34ef1
Manuel Faure <manuelfaure26190@gmail.com>
2023-06-28 17:25:55 +0200

    save

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,8 +1,19 @@
-
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import imgAbout from '../../assets/img-about.jpg';
+import '../../styles/About.css'; 
 
 function About (){
     return (
-        <h1>About Test</h1>
+        <section>
+            <Header />
+            <div className="div-about">
+                <img className="about-img" src={imgAbout} alt="photographie de haute montagnes"/>
+            </div>
+            <div></div>
+            <Footer />
+        </section>     
+        
     )
 }
 
