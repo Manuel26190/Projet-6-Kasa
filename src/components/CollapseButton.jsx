f3f3de1171755a222a130abf088c42d97333b352
Manuel Faure <manuelfaure26190@gmail.com>
2023-06-29 17:15:25 +0200

    save

--- src/components/CollapseButton.jsx
+++ src/components/CollapseButton.jsx
@@ -1,50 +1,19 @@
 import { useState } from "react";
 
-function CollapseButton () {
+function CollapseButton ({entitled, text}) {
     const [isOpen, setIsOpen] = useState(true);
     
-    const fiabilityEntiled = "fiabilité";
-    const fiabilityText = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.";
-    const respectEntiled = "Respect";
-    const respectText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";  
-    const serviceEntiled = "Service";
-    const serviceText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";
-    const securityEntiled = "Securité"
-    const securityText ="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."; 
-
-    return isOpen ? (                   
-            <section>
+        return isOpen ? (                   
+            
                 <div className="div-collapse">
-                    <button className="button-collapse" onClick={() => setIsOpen (false)}>{fiabilityEntiled}</button>
+                    <button className="button-collapse" onClick={() => setIsOpen (false)}>{entitled}</button>
                     <div className="div-text-collapse">
-                        <p className="collapse-text">{fiabilityText}</p>
+                        <p className="collapse-text">{text}</p>
                     </div>                 
-                </div>               
-                <div className="div-collapse">
-                    <button className="button-collapse" onClick={() => setIsOpen (false)}>{respectEntiled}</button>
-                    <div className="div-text-collapse">
-                        <p className="collapse-text">{respectText}</p>
-                    </div>                 
-                </div>
-                <div className="div-collapse">
-                    <button className="button-collapse" onClick={() => setIsOpen (false)}>{serviceEntiled}</button>
-                    <div className="div-text-collapse">
-                        <p className="collapse-text">{serviceText}</p>
-                    </div>                 
-                </div>
-                <div className="div-collapse">
-                    <button className="button-collapse" onClick={() => setIsOpen (false)}>{securityEntiled}</button>
-                    <div className="div-text-collapse">
-                        <p className="collapse-text">{securityText}</p>
-                    </div>                 
-                </div>
-            </section>                           
+                </div>                                       
     ) : (
             <div>
-                <button className="button-fiability" onClick={() => setIsOpen (true)}>{fiabilityEntiled}</button>
-                <button className="button-fiability" onClick={() => setIsOpen (true)}>{respectEntiled}</button>
-                <button className="button-fiability" onClick={() => setIsOpen (true)}>{serviceEntiled}</button>
-                <button className="button-fiability" onClick={() => setIsOpen (true)}>{securityEntiled}</button>
+                <button className="button-fiability" onClick={() => setIsOpen (true)}>{entitled}</button>                
             </div>
     ) 
 };
--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -4,7 +4,17 @@ import imgAbout from '../../assets/img-about.jpg';
 import CollapseButton from "../../components/CollapseButton";
 import '../../styles/About.css'; 
 
-function About ({entiled, text}){
+function About (){
+
+    const fiabilityEntitled = "fiabilité";
+    const fiabilityText = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.";
+    const respectEntitled = "Respect";
+    const respectText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";  
+    const serviceEntitled = "Service";
+    const serviceText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";
+    const securityEntitled = "Securité"
+    const securityText ="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."; 
+
     
     return (
         <div>
@@ -12,12 +22,12 @@ function About ({entiled, text}){
             <div className="div-about">
                 <img className="about-img" src={imgAbout} alt="photographie de haute montagnes"/>
             </div>
-            <CollapseButton entiled={entiled} text={text} />            
+            <CollapseButton entitled={fiabilityEntitled} text={fiabilityText} />            
+            <CollapseButton entitled={respectEntitled} text={respectText} />            
+            <CollapseButton entitled={serviceEntitled} text={serviceText} />            
+            <CollapseButton entitled={securityEntitled} text={securityText} />            
             <Footer />
-        </div>
-            
-            
-        
+        </div>        
     )
 }
 
