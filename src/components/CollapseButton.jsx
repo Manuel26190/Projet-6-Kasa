88ee360a0391294dd756c7740ea72ca17a6dd4e6
Manuel Faure <manuelfaure26190@gmail.com>
2023-06-30 14:21:06 +0200

    save

--- src/components/CollapseButton.jsx
+++ src/components/CollapseButton.jsx
@@ -5,9 +5,8 @@ import ChevronLogo from '../assets/logo-chevron.jpg';
 function CollapseButton ({entitled, text}) {
     const [isOpen, setIsOpen] = useState(true);
     
-        return isOpen ? (                   
-            
-            <div className="collapse-div">
+        return isOpen ? (             
+                <div className="collapse-div">
                 <button className="open-collapse-button" onClick={() => setIsOpen (false)}>
                     {entitled}
                     <img src={ChevronLogo} alt="logo-chevron" className="chevron-logo"/>
@@ -15,14 +14,14 @@ function CollapseButton ({entitled, text}) {
                 <div className="collapse-div-text">
                     <p className="collapse-text">{text}</p>
                 </div>                 
-            </div>                                       
-    ) : (
-            <div className="collapse-div">
+            </div>                                                   
+    ) : (            
+                <div className="collapse-div">
                 <button className="close-collapse-button" onClick={() => setIsOpen (true)}>
                     {entitled}
                     <img src={ChevronLogo} alt="logo-chevron" className="chevron-logo"/>
                 </button>                
-            </div>
+            </div>           
     ) 
 };
 
