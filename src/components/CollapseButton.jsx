dd015d087e7d34a350d18291c90d386e99d053dc
Manuel Faure <manuelfaure26190@gmail.com>
2023-07-01 17:37:53 +0200

    save

--- src/components/CollapseButton.jsx
+++ src/components/CollapseButton.jsx
@@ -1,27 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import '../styles/CollapseButton.css';
 import ChevronLogo from '../assets/logo-chevron.jpg';
 
-function CollapseButton ({entitled, text}) {
+function CollapseButton ({buttonText, content}) {    
     
-    //const [rotation, setRotation] = useState(false); 
     const [isOpen, setIsOpen] = useState(false); 
-    //const setIsOp = "seIsOpen(false)";    
+       
     
         return isOpen ? (             
                 <div className="collapse-div">
                 <button className="open-collapse-button" onClick={() => {setIsOpen (false)} } >
-                    {entitled}
+                    {buttonText}
                     <img src={ChevronLogo} alt="logo-chevron" className="chevron-logo"/>
                 </button>
                 <div className="collapse-div-text">
-                    <p className="collapse-text">{text}</p>
+                    <p className="collapse-text">{content}</p>
                 </div>                 
             </div>                                                   
     ) : (            
                 <div className="collapse-div">
                 <button className="close-collapse-button" onClick={() => setIsOpen (true)}>
-                    {entitled}
+                    {buttonText}
                     <img src={ChevronLogo} alt="logo-chevron" className="chevron-logo"/>
                 </button>                
             </div>           
--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -4,24 +4,38 @@ import '../../styles/About.css';
 
 function About (){
 
-    const fiabilityEntitled = "Fiabilité";
-    const fiabilityText = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.";
-    const respectEntitled = "Respect";
-    const respectText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";  
-    const serviceEntitled = "Service";
-    const serviceText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";
-    const securityEntitled = "Securité"
-    const securityText ="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."; 
+    const contentList = [
+        {
+              buttonText: 'Fiabilité',
+              content: 'Les annonces postées sur Kasa garantissent une fiabilité totale.Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+        },
+        {
+              buttonText: 'Respect',
+              content: 'La bienveillance fait partie des valeurs fondatrices de Kasa.Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+        },
+        {
+              buttonText: 'Service',
+              content: 'Chez Kasa, nous nous engageons à fournir un service exceptionnel à nos utilisateurs. Notre équipe est dédiée à vous offrir une expérience sans pareille, en répondant rapidement à vos besoins et en vous fournissant un support personnalisé à chaque étape de votre parcours sur notre plateforme. Nous nous efforçons de rendre votre expérience de location de logement aussi fluide et agréable que possible.',
+        },
+        {
+              buttonText: 'Sécurité',
+              content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes",
+        },
+  ];
     
     return (
         <div>            
             <div className="div-about">
                 <img className="about-img" src={imgAbout} alt="photographie de haute montagnes"/>
             </div>
-            <CollapseButton entitled={fiabilityEntitled} text={fiabilityText} />            
-            <CollapseButton entitled={respectEntitled} text={respectText} />            
-            <CollapseButton entitled={serviceEntitled} text={serviceText} />            
-            <CollapseButton entitled={securityEntitled} text={securityText} />            
+            {contentList.map((content) => (
+                <CollapseButton
+                    buttonText = {content.buttonText}
+                    content = {content.content}
+                />
+            ))}
+                       
+                        
         </div>        
     )
 }
