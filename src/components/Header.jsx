c1eb40891894ae381054c15cfc6d9e4ded84df13
Manuel Faure <manuelfaure26190@gmail.com>
2023-07-04 17:43:38 +0200

    save

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,36 @@ import '../styles/Header.css';
 import Redlogo from '../assets/LOGO-kasa-red.svg';
 import '../pages/Home/HomePage.jsx';
 import '../pages/About/AboutPage.jsx';
+import React, { useState } from 'react';
+
+
+  
 
 function Header() {
+	const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+	
 	return (
 		<div className='header-container'>
-			<div className='kasa-Header'>			
-				<img src={Redlogo} alt='logo-Kasa'/>
+			<div className='kasa-Header'>
+				<Link to='/'>
+					<img src={Redlogo} alt='logo-Kasa'
+					className={isActive ? 'nav-link-home active' : 'nav-link-home active'} 
+					onClick={handleClick}
+					/>					
+				</Link>			
+				
 				<nav className='kasa-nav'>
 					<ul className='kasa-ul'>
-						<Link to='/' className='nav-link'>Accueil</Link>
-						<Link to="/about" className='nav-link'>A Propos</Link>										
+						<Link to='/' className={isActive === false ? 'nav-link-home active' : 'nav-link-home'}
+        				onClick={handleClick} 
+						>Accueil</Link>
+						<Link to="/about" className={isActive === true ? 'nav-link-about open' : 'nav-link-about'}
+						onClick={handleClick}
+						>A Propos</Link>										
 					</ul>
 				</nav>		
 			</div>
