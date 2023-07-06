f0cbe0822db9a09bfc22b40602dbbc68821c92e6
Manuel Faure <manuelfaure26190@gmail.com>
2023-07-06 17:58:09 +0200

    save

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,6 +2,7 @@ import CollapseButton from "./CollapseButton";
 import '../styles/Info.css';
 import React from "react";
 import DropdownList from "./DropdownList";
+import Rating from "./Rating";
 
 function Info ({descriptionButton, 
                 descriptionText, 
@@ -23,12 +24,15 @@ function Info ({descriptionButton,
                 </div>                
             </div>
             <p className="location">{location}</p>
-            <div className="div-tag-rating">
-                <div className="div-tag">{tags}</div>
-                <div></div>
+            <div className="div-tags-rating">
+                <div className="div-tags">
+                    {tags.map((tag, index) => (
+                        <p key={index}>{tag}</p>
+                    ))}
 
-            </div>
-
+                </div>
+                <Rating rating={rating} />               
+            </div>           
             <div className="info-collapse-div">            
                     <CollapseButton
                      buttonText={descriptionButton}
---
+++ src/components/Rating.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+function Rating () {
+    return (
+        <div className="div-tags-rating">
+                <FontAwesomeIcon icon={faStar} />
+        </div>
+    )
+}
+
+export default Rating
