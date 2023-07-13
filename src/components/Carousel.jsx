52797e7e0a2634843f263d00f108ced9c09b66a3
Manuel Faure <manuelfaure26190@gmail.com>
2023-07-13 15:55:14 +0200

    save

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -35,8 +35,7 @@ L'image affichée est basée sur l'URL correspondante à l'index currentSlide da
         <p>{currentSlide + 1}/{pictures.length}</p>
         <button className={pictures.length === 1 ? 'next-button active' : 'next-button'} onClick={nextSlide}>
             <FontAwesomeIcon icon={faChevronRight} />
-        </button>
-        
+        </button>        
       </div>
     </div>
   );
